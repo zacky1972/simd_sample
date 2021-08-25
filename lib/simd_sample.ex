1d5e7dbcfca9990fdd7aed724ea67ab956787a5f
Susumu Yamazaki <zacky1972@gmail.com>
2021-08-25 17:54:36 +0900

    create a NIF function

--- lib/simd_sample.ex
+++ lib/simd_sample.ex
@@ -1,7 +1,22 @@
 defmodule SimdSample do
+  require Logger
+
   @moduledoc """
   Documentation for SimdSample.
   """
+  @on_load :load_nif
+
+  def load_nif do
+    nif_file = '#{Application.app_dir(:simd_sample, "priv/lib/libnif")}'
+
+    case :erlang.load_nif(nif_file, 0) do
+      :ok -> :ok
+      {:error, {:reload, _}} -> :ok
+      {:error, reason} -> Logger.error("Failed to load NIF: #{inspect(reason)}")
+    end
+  end
+
+  def test(), do: raise("NIF test/0 not implemented")
 
   @doc """
   Hello world.
--- native/libnif.c
+++ native/libnif.c
@@ -1,1 +1,14 @@
+#include <stdlib.h>
 #include <erl_nif.h>
+
+static ERL_NIF_TERM test(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
+{
+    return enif_make_atom(env, "ok");
+}
+
+static ErlNifFunc nif_funcs[] =
+{
+    {"test", 0, test}
+};
+
+ERL_NIF_INIT(Elixir.SimdSample, nif_funcs, NULL, NULL, NULL, NULL)
