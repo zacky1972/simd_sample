dbdbf01d7d1cb7788a98aa2ab26297e12cc47294
Susumu Yamazaki <zacky1972@gmail.com>
2021-08-26 03:58:34 +0900

    add include intrinsics

--- native/libnif.c
+++ native/libnif.c
@@ -4,6 +4,16 @@
 #include "alignas.h"
 #include "alignof.h"
 
+#ifdef __arm__
+#include <arm_neon.h>
+#elif __x86_64__
+#ifdef _MSC_VER
+#include <intrin.h>
+#else
+#include <x86intrin.h>
+#endif // _MSC_VER
+#endif
+
 _Alignas(alignof(unsigned int)) unsigned int c[256];
 
 static ERL_NIF_TERM test(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
