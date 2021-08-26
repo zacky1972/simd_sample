3262146f325e621ea99695cfdbf09187e40a0f15
Susumu Yamazaki <zacky1972@gmail.com>
2021-08-27 07:37:15 +0900

    create mm256_unsigned_extend_epi8_to_epi32 and describe doc of it

--- native/libnif.c
+++ native/libnif.c
@@ -70,17 +70,69 @@ void init_half_mask()
     half_mask_256 = _mm256_load_ps(half_f);
 }
 
+/*
+
+Description
+    Unsigned extend packed 8bit integers from a to packed 32-bit integers, 
+    and store the result in dst.
+
+Operation
+    dst[7:0]     = a[7:0]
+    dst[31:8]    = 0
+    dst[39:32]   = a[15:8]
+    dst[63:40]   = 0
+    dst[71:64]   = a[23:16]
+    dst[95:72]   = 0
+    dst[103:96]  = a[31:24]
+    dst[127:104] = 0
+    dst[135:128] = a[39:32]
+    dst[159:136] = 0
+    dst[167:160] = a[47:40]
+    dst[191:168] = 0
+    dst[199:192] = a[55:48]
+    dst[223:200] = 0
+    dst[231:224] = a[63:56]
+    dst[MAX:232] = 0
+
+*/
+
+extern inline __m256i mm256_unsigned_extend_epi8_to_epi32(__m256i a);
+
+inline __m256i mm256_unsigned_extend_epi8_to_epi32(__m256i a)
+{
+    __m256i shuffled_lower_256[2];
+    shuffled_lower_256[0] = _mm256_shuffle_epi8(a, shuffle_epi8_m256[0]);
+    shuffled_lower_256[1] = _mm256_shuffle_epi8(a, shuffle_epi8_m256[1]);
+    shuffled_lower_256[1] = _mm256_permutevar8x32_epi32(shuffled_lower_256[1], permutevar8x32_epi32_m256[0]);
+    return _mm256_max_epi32(shuffled_lower_256[0], shuffled_lower_256[1]);
+}
+
+/*
+
+    result[index][7:0]     = value[index][7:0]
+    result[index][31:8]    = 0
+    result[index][39:32]   = value[index][15:8]
+    result[index][63:40]   = 0
+    result[index][71:64]   = value[index][23:16]
+    result[index][95:72]   = 0
+    result[index][103:96]  = value[index][31:24]
+    result[index][127:104] = 0
+    result[index][135:128] = value[index][39:32]
+    result[index][159:136] = 0
+    result[index][167:160] = value[index][47:40]
+    result[index][191:168] = 0
+    result[index][199:192] = value[index][55:48]
+    result[index][223:200] = 0
+    result[index][231:224] = value[index][63:56]
+    result[index][MAX:232] = 0
+
+*/
+
 extern inline void mm256_epi32_extract_epi8x(int index, __m256i *value, __m256i *result);
 
 inline void mm256_epi32_extract_epi8x(int index, __m256i *value, __m256i *result)
 {
-    __m256i shuffled_lower_256[2];
-    __m256i shuffled_256[1];
-    shuffled_lower_256[0] = _mm256_shuffle_epi8(value[index], shuffle_epi8_m256[0]);
-    shuffled_lower_256[1] = _mm256_shuffle_epi8(value[index], shuffle_epi8_m256[1]);
-    shuffled_lower_256[1] = _mm256_permutevar8x32_epi32(shuffled_lower_256[1], permutevar8x32_epi32_m256[0]);
-    shuffled_256[0] = _mm256_max_epi32(shuffled_lower_256[0], shuffled_lower_256[1]);
-    _mm256_storeu_si256(&result[index], shuffled_256[0]);
+    _mm256_storeu_si256(&result[index], mm256_unsigned_extend_epi8_to_epi32(value[index]));
 }
 
 extern inline __m256i mm256_round_cvtps_epi32(__m256 value);
