ebf0f4b1fd641b7874394f9678995fb2968608fa
Susumu Yamazaki <zacky1972@gmail.com>
2021-08-27 12:43:14 +0900

    fix the issue that cannot run on the target due to init_basic_intrinsics in basic_intrinsics

--- native/basic_intrinsics.c
+++ native/basic_intrinsics.c
@@ -2,6 +2,10 @@
 
 void init_basic_intrinsics()
 {
+#if __x86_64__ 
+#if defined(HAVE_AVX_INSTRUCTIONS) && defined(HAVE_AVX2_INSTRUCTIONS)
     init_mm256_unsigned_extend_epi8_to_epi32();
     init_mm256_round_cvtps_epi32();
+#endif
+#endif
 }
