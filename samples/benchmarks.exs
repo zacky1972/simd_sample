8a52f3de635fc654e409494cb4a51e70725df1ec
Susumu Yamazaki <zacky1972@gmail.com>
2021-08-28 15:10:27 +0900

    implement benchmarks of nx_aos32 and nx_aos16

--- samples/benchmarks.exs
+++ samples/benchmarks.exs
@@ -1,12 +1,16 @@
 {:ok, result} = SimdSample.aos0_test()
 {:ok, ^result} = SimdSample.soa0_test()
 {:ok, ^result} = SimdSample.soa1_test()
+^result = SimdSample.nx_aos32_test()
+^result = SimdSample.nx_aos16_test()
 
 Benchee.run(
   %{
     "aos0" => fn -> SimdSample.aos0_test() end,
     "soa0" => fn -> SimdSample.soa0_test() end,
-    "soa1" => fn -> SimdSample.soa1_test() end
+    "soa1" => fn -> SimdSample.soa1_test() end,
+    "nx_aos32" => fn -> SimdSample.nx_aos32_test() end,
+    "nx_aos16" => fn -> SimdSample.nx_aos16_test() end
   }
 ) \
 |> then(fn _ -> :ok end)
