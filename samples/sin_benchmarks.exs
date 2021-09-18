e8bc5551249e5b20c7e1093503a7fe3651104f68
Susumu Yamazaki <zacky1972@gmail.com>
2021-09-19 01:36:49 +0900

    change size of vector

--- samples/sin_benchmarks.exs
+++ samples/sin_benchmarks.exs
@@ -1,6 +1,6 @@
 
-input_f32 = Nx.iota({256}, type: {:f, 32})
-input_f16 = Nx.iota({256}, type: {:f, 16})
+input_f32 = Nx.iota({0x1000000}, type: {:f, 32})
+# input_f16 = Nx.iota({0x1000000}, type: {:f, 16})
 
 result = Nx.sin(input_f32)
 0.0 = Nx.subtract(result, SimdSample.Sin.sin32(input_f32)) |> Nx.sum() |> Nx.to_scalar()
@@ -11,7 +11,7 @@ Benchee.run(
     "sin32_1" => fn -> SimdSample.Sin.sin32_1(input_f32) end,
     "sin32_0" => fn -> SimdSample.Sin.sin32(input_f32) end,
     "nx_32" => fn -> Nx.sin(input_f32) end,
-    "nx_16" => fn -> Nx.sin(input_f16) end
+#    "nx_16" => fn -> Nx.sin(input_f16) end
   }
 ) \
 |> then(fn _ -> :ok end)
